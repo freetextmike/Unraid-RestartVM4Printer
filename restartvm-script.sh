b1b516d6732332a8f3e7703408e3f36d2edb6ad0
freetextmike <63688198+freetextmike@users.noreply.github.com>
2023-03-18 00:56:12 +0800

    Update restartvm-script.sh
    added extra parameters for the improvement of the script

--- restartvm-script.sh
+++ restartvm-script.sh
@@ -1,21 +1,32 @@
-if lsusb | grep "04a9:183c" > /dev/null
+# Check if the VM is running
+if virsh list | grep "Linux" | grep "running" > /dev/null
 then
-  echo "USB device is connected. Skipping shutdown."
+  # VM is running
+  echo "VM Linux is running."
+  
+  # Check if the USB device is connected
+  if lsusb | grep "04a9:183c" > /dev/null
+  then
+    # USB device is connected, no need to do anything
+    echo "USB device is connected. Skipping VM shutdown."
+  else
+    # USB device is not connected, shut down the VM
+    echo "USB device is not connected. Shutting down VM Linux."
+    virsh shutdown Linux
+  fi
 else
-  # Stop the VM
-  virsh shutdown Linux
-
-  # Wait for the printer to be power cycled
-  echo "Please power cycle the printer."
-  while [ true ]
-  do
-    if lsusb | grep "04a9:183c" > /dev/null
-    then
-      break
-    fi
-    sleep 5
-  done
-
-  # Start the VM
-  virsh start Linux
+  # VM is not running
+  echo "VM Linux is not running."
+  
+  # Check if the USB device is connected
+  if lsusb | grep "04a9:183c" > /dev/null
+  then
+    # USB device is connected, wait for 10 seconds and start the VM
+    echo "USB device is connected. Waiting for 10 seconds before starting VM Linux."
+    sleep 10
+    virsh start Linux
+  else
+    # USB device is not connected, no need to do anything
+    echo "USB device is not connected. Skipping VM start."
+  fi
 fi
